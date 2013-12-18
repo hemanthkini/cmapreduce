40e378a4862b5263335bf4a2505a8b7619107838
Brandon Lum <brandon@Brandons-MacBook-Air.local>
2013-12-18 17:24:51 -0500

    Change map spec and added comments

--- hof.c
+++ hof.c
@@ -2,14 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define map(size,type1,type2,inarr, fn) (map_fun(size, sizeof(type1), sizeof(type2),(void*)inarr,(genfnptr*)fn))
 
+// Typedef for general void* fun(void*) function
 typedef void* genfnptr(void*);
 
-void* map_fun(size_t arrlen, size_t fromsize, size_t tosize, void* inputarr,  genfnptr* mapfn)
+/* Definition of map
+ *
+ * map(size, in_type, out_type, in_array, out_array, fn)
+ *
+ * size - size of array
+ * in_type - type of input array
+ * out_type - type of output array
+ * in_array - address to input array
+ * out_array - address to store output array
+ * fn - function to map
+ *
+ */
+
+
+#define map(size,type1,type2,inarr,outarr, fn) (map_fun(size, sizeof(type1), sizeof(type2),(void*)inarr,(void*) outarr, (genfnptr*)fn))
+void map_fun(size_t arrlen, size_t fromsize, size_t tosize, void* inputarr,  void* outputarr, genfnptr* mapfn)
 {
-    // Prepare output array
-    void* outputarr = malloc(tosize * arrlen);
 
     // Optimize acc in registers
     int tosizeacc=0;
@@ -30,12 +43,17 @@ void* map_fun(size_t arrlen, size_t fromsize, size_t tosize, void* inputarr,  ge
         fromsizeacc += fromsize;
         tosizeacc += tosize;
     }
-    return outputarr;
 }
 
 
 
 
+/**
+ *
+ * ALL DA TESTING FUNCTIONS IN DA HOUSE
+ *
+ *
+ **/
 int* square(int* a)
 {
     int* result=malloc(sizeof(int));
@@ -76,7 +94,9 @@ int main(int argc, char **argv)
 
     printf("Test 1: square\n");
     int intarr[10] = {0,1,2,3,4,5,6,7,8,9};
-    int *retnarr = map(10,int,int,intarr,&square);
+    int retnarr[10];
+
+    map(10,int,int,intarr, retnarr, &square);
 
     int i=0;
     for (i=0;i<10;i++)
@@ -86,11 +106,18 @@ int main(int argc, char **argv)
     printf("END\n\n");
 
 
+
+
+
+
+
+
     // Test 2
     // Output 1/x
     //
     printf("Test 2: 1/x\n");
-    float *retnarr2 = map(10,int,float,intarr,&fraction);
+    float retnarr2[10];
+    map(10,int,int,intarr, retnarr2, &fraction);
 
     i=0;
     for (i=0;i<10;i++)
@@ -99,11 +126,19 @@ int main(int argc, char **argv)
     }
     printf("END\n\n");
 
+
+
+
+
+
+
     // Test 3
     // Times 2 struct
     //
+    struct tup retnarr3[10];
+
     printf("Test 3: orig and orig*2 struct\n");
-    struct tup *retnarr3 = map(10,int,struct tup, intarr, &evaltup);
+    map(10,int,struct tup, intarr, retnarr3, &evaltup);
 
     i=0;
     for (i=0;i<10;i++)
